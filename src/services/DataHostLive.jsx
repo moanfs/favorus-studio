ce5515fb84b59e878c6e1d21d7c975a895412298
moan <moanfs1@gmail.com>
2024-06-07 07:23:37 +0700

    hot fix (host live) : fix read data host live

--- src/services/DataHostLive.jsx
+++ src/services/DataHostLive.jsx
@@ -1,9 +1,10 @@
 import axios from 'axios';
-const BASE_URL = import.meta.env.VITE_REST_API;
+const client_key = import.meta.env.VITE_SPREADSHEET_KEY
+const client_sheet = import.meta.env.VITE_SPREADSHEET_SHEET
 
 export const fetchDataHostFromSpreadsheet  = async () => {
       try {
-        const response = await axios.get(`${BASE_URL}`);
+        const response = await axios.get(`https://sheets.googleapis.com/v4/spreadsheets/${client_sheet}/values/Sheet1?key=${client_key}`);
         const rows = response.data.values;
         const headers = rows[0];
         const dataRows = rows.slice(1).map(row => {
